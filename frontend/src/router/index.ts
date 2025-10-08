e4a8f0ee633f5555ec970480a44e59c542df5fc9
denis <ddo@rusavtomatika.bizml.ru>
2025-10-08 14:52:23 +0300

    add guard to router, frontend

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,13 +1,36 @@
 import { createWebHistory, createRouter } from 'vue-router';
+import { useAuthStore } from '../store/auth-store';
 import HomePage from '../pages/HomePage.vue';
 import AuthPage from '../pages/AuthPage.vue';
 
 const routes = [
-  { path: '/', component: HomePage },
-  { path: '/auth', component: AuthPage }
+  {
+    path: '/',
+    component: HomePage,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/auth',
+    component: AuthPage,
+    meta: { requiresAuth: true }
+  }
 ]
 
-export default createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes,
 })
+
+router.beforeEach((to, from, next) => {
+  const store = useAuthStore()
+
+  if(to.meta.requiresAuth && !store.user.access_token) {
+    next('/auth')
+  } else if(to.meta.requiresAuth && store.user.access_token) {
+    next('/')
+  } else {
+    next()
+  }
+})
+
+export default router
