bb2c8537abf02715f6fe40aae6c6a4ffec596529
denis <ddo@rusavtomatika.bizml.ru>
2025-10-09 16:05:38 +0300

    fix verstka header(frontend)

--- frontend/src/components/HeaderItem.vue
+++ frontend/src/components/HeaderItem.vue
@@ -1,13 +1,12 @@
 <template>
-  <header class="header-item">
-    <div class="header__user">
-      <RouterLink to="/profile" class="user-link">
-        <font-awesome-icon icon="fa-solid fa-user-circle" class="user__icon"/>
-        <p class="user__mail">{{ store.user.email }}</p>
-      </RouterLink>
-      <RouterLink to="/" class="base-link" style="margin-bottom: 5px;">Главная</RouterLink>
-      <button class="base-link" @click="logout">Выйти</button>
-    </div>
+  <header class="header__user">
+    <RouterLink to="/profile" class="user-link">
+      <font-awesome-icon icon="fa-solid fa-user-circle" class="user__icon"/>
+      <p class="user__info">{{ store.user.email }}</p>
+      <p class="user__info">{{ store.user.name }}</p>
+    </RouterLink>
+    <RouterLink to="/" class="base-link" style="margin-bottom: 5px;">Главная</RouterLink>
+    <button class="base-link" @click="logout">Выйти</button>
   </header>
 </template>
 
@@ -25,51 +24,44 @@ async function logout(): Promise<void> {
 </script>
 
 <style lang="scss" scoped>
-.header-item {
+.header__user {
   position: fixed;
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  width: 100%;
   top: 0;
-  left: 0;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  background: var(--base-white);
+  box-shadow: 1px 1px 5px 1px var(--base-black);
+  border-radius: 5px;
+  width: 150px;
+  padding: 10px;
 
-  .header__user {
+  .user-link {
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: flex-start;
-    background: var(--base-white);
-    box-shadow: 1px 1px 5px 1px var(--base-black);
-    border-radius: 5px;
-    width: 150px;
-    padding: 10px;
+    margin-bottom: 15px;
+    text-decoration: none;
+    width: 100%;
+    gap: 5px;
 
-    .user-link {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      margin-bottom: 15px;
-      text-decoration: none;
+    .user__icon {
+      width: 45px;
+      height: 45px;
+      color: var(--base-black);
+    }
+
+    .user__info {
+      font-size: 12px;
+      font-weight: 400;
+      color: var(--base-black);
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      text-align: center;
       width: 100%;
-      gap: 5px;
-
-      .user__icon {
-        width: 45px;
-        height: 45px;
-        color: var(--base-black);
-      }
-
-      .user__mail {
-        font-size: 12px;
-        font-weight: 400;
-        color: var(--base-black);
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        text-align: center;
-        width: 100%;
-      }
     }
   }
 }
