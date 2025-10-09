92a2b7fbf452e5a225e586ba4043ce82ade1fc07
denis <ddo@rusavtomatika.bizml.ru>
2025-10-09 16:29:24 +0300

    добавил анимацию переходов между страницами(frontend)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,23 @@
 <template>
-  <RouterView />
+  <RouterView v-slot="{ Component }">
+    <Transition name="fade" mode="out-in">
+      <component :is="Component" />
+    </Transition>
+  </RouterView>
 </template>
 
 <script setup lang="ts">
 
 </script>
+
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
