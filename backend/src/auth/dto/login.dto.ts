42b052ac2c3bb5c7abf1439cc9676269e275f461
denis <ddo@rusavtomatika.bizml.ru>
2025-10-13 13:41:05 +0300

    add min,max lenght auth dto(backend)

--- backend/src/auth/dto/login.dto.ts
+++ backend/src/auth/dto/login.dto.ts
@@ -1,11 +1,14 @@
-import { IsString, IsNotEmpty } from "class-validator";
+import { IsString, IsNotEmpty, MinLength, MaxLength } from "class-validator";
 
 export class Login {
   @IsString({ message: 'Почта должна быть строкой' })
   @IsNotEmpty({ message: 'Почта не может быть пустой' })
+  @MinLength(10, { message: 'Почта не может быть короче 10 символов' })
   email: string;
 
   @IsString({ message: 'Пароль должен быть строкой' })
   @IsNotEmpty({ message: 'Пароль не может быть пустым' })
+  @MinLength(5, { message: 'Пароль не может быть короче 5 символов' })
+  @MaxLength(50, { message: 'Пароль не может быть длинее 50 символов' })
   password: string;
 }
--- backend/src/auth/dto/register.dto.ts
+++ backend/src/auth/dto/register.dto.ts
@@ -1,15 +1,20 @@
-import { IsString, IsNotEmpty, IsOptional } from "class-validator";
+import { IsString, IsNotEmpty, IsOptional, MinLength, MaxLength } from "class-validator";
 
 export class Register {
   @IsString({ message: 'Почта должна быть строкой' })
   @IsNotEmpty({ message: 'Почта не может быть пустой' })
+  @MinLength(10, { message: 'Почта не может быть короче 10 символов' })
   email: string;
 
   @IsString({ message: 'Имя должно быть строкой' })
+  @MinLength(2, { message: 'Имя не может быть короче 2 символов' })
+  @MaxLength(20, { message: 'Имя не может быть длинее 20 символов' })
   @IsOptional()
   name?: string;
 
   @IsString({ message: 'Пароль должен быть строкой' })
   @IsNotEmpty({ message: 'Пароль не может быть пустым' })
+  @MinLength(5, { message: 'Пароль не может быть короче 5 символов' })
+  @MaxLength(50, { message: 'Пароль не может быть длинее 50 символов' })
   password: string;
 }
